af73e99649061ef87da5d2d430d863154de34dba
KuzyaWkk <m.kuzmych@dev-branch.com>
2024-02-08 17:37:32 +0200

    Added views for Content by category, Lates News, Must Read, You might also like

---
+++ web/modules/custom/kenny_core/src/Service/CategoryContentInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\kenny_core\Service;
+interface CategoryContentInterface {
+
+  /**
+   * @param $term_name
+   *   The name of taxonomy term.
+   *
+   * @return int
+   */
+  public function getContent($term_name);
+
+}
