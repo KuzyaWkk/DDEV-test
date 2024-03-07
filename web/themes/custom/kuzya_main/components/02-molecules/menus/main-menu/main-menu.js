f650d52f23276dd1e1bdb8e9a9e1562386f291da
KuzyaWkk <m.kuzmych@dev-branch.com>
2024-03-07 17:57:26 +0200

    7645 - Added once for main-menu.js. Fixed some styles. Removed view taxonomy term. Edited suggestion for mini-pager

--- web/themes/custom/kuzya_main/components/02-molecules/menus/main-menu/main-menu.js
+++ web/themes/custom/kuzya_main/components/02-molecules/menus/main-menu/main-menu.js
@@ -1,52 +1,52 @@
-Drupal.behaviors.mainMenu = {
-  attach(context) {
-    const toggleExpand = context.getElementById('toggle-expand');
-    const menu = context.getElementById('main-nav');
-    // const social = context.getElementById('social-bar-mobile');
-    const branding = context.getElementById('branding');
-    const social = context.getElementById('social-desc');
-    const socialMobile = context.getElementById('social-mobile');
-    const search = context.getElementById('search-desc');
-    const main = document.querySelector('.main');
-    const contentBottom = document.querySelector('.content-bottom');
-    const aboutUs = document.querySelector('.about-us');
-    const footer = document.querySelector('.footer');
-    const header = document.querySelector('.header');
+(function (Drupal, once) {
+  Drupal.behaviors.kuzyaMainMenuBehavior = {
+    attach(context) {
+      const headerRegion = '.header';
+      const name = 'kuzyaMainMenuBehavior';
+      const elements = once(name, headerRegion, context);
+      elements.forEach(function (element) {
+        const expandMenu = element.querySelectorAll('.expand-sub');
+        const toggleExpand = element.querySelector('.toggle-expand');
+        const menu = element.querySelector('.main-nav');
+        const branding = element.querySelector('.header__branding');
+        const social = element.querySelector('.header__social-bar');
+        const socialMobile = element.querySelector(
+          '.header__social-bar-mobile',
+        );
+        const search = element.querySelector('.header__search-menu');
+        const main = document.querySelector('.main');
+        const contentBottom = document.querySelector('.content-bottom');
+        const aboutUs = document.querySelector('.about-us');
+        const footer = document.querySelector('.footer');
+        const open = element.querySelector('.toggle-expand__open');
+        const close = element.querySelector('.toggle-expand__close');
+        const header = element;
 
-    const open = toggleExpand.querySelector('.toggle-expand__open');
-    const close = toggleExpand.querySelector('.toggle-expand__close');
-
-    if (menu) {
-      const expandMenu = menu.getElementsByClassName('expand-sub');
-
-      // Mobile Menu Show/Hide.
-      toggleExpand.addEventListener('click', (e) => {
-        toggleExpand.classList.toggle('toggle-expand--open');
-        menu.classList.toggle('main-nav--open');
-        social.classList.toggle('hide');
-        socialMobile.classList.toggle('hide');
-        branding.classList.toggle('hide');
-        search.classList.toggle('hide');
-        main.classList.toggle('hide');
-        contentBottom.classList.toggle('hide');
-        aboutUs.classList.toggle('hide');
-        footer.classList.toggle('hide');
-        open.classList.toggle('hide');
-        close.classList.toggle('show');
-        header.classList.toggle('header__mobile');
-        e.preventDefault();
-      });
-
-      // Expose mobile sub menu on click.
-      Array.from(expandMenu).forEach((item) => {
-        item.addEventListener('click', (e) => {
-          const menuItem = e.currentTarget;
-          const subMenu = menuItem.nextElementSibling;
-
-          menuItem.classList.toggle('expand-sub--open');
-          subMenu.classList.toggle('main-menu--sub-open');
+        toggleExpand.addEventListener('click', function (e) {
+          e.preventDefault();
+          toggleExpand.classList.toggle('toggle-expand--open');
+          menu.classList.toggle('main-nav--open');
+          social.classList.toggle('hide');
+          socialMobile.classList.toggle('hide');
+          branding.classList.toggle('hide');
+          search.classList.toggle('hide');
+          main.classList.toggle('hide');
+          contentBottom.classList.toggle('hide');
+          aboutUs.classList.toggle('hide');
+          footer.classList.toggle('hide');
+          open.classList.toggle('hide');
+          close.classList.toggle('show');
+          header.classList.toggle('header__mobile');
+        });
+        expandMenu.forEach((item) => {
+          item.addEventListener('click', (e) => {
+            const menuItem = e.currentTarget;
+            const subMenu = menuItem.nextElementSibling;
+            menuItem.classList.toggle('expand-sub--open');
+            subMenu.classList.toggle('main-menu--sub-open');
+          });
         });
       });
-    }
-  },
-};
+    },
+  };
+}(Drupal, once));
