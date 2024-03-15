5c3d49b9e1b72cab0f6b92cba778aff0edc22ca8
KuzyaWkk <m.kuzmych@dev-branch.com>
2024-03-15 12:12:38 +0200

    7717 - Added some images for the weather block. Added cache for weather data. Added hook_update for hook_schema

--- web/modules/custom/weather_api/src/Plugin/Block/WeatherApiBlock.php
+++ web/modules/custom/weather_api/src/Plugin/Block/WeatherApiBlock.php
@@ -4,6 +4,7 @@ namespace Drupal\weather_api\Plugin\Block;
 
 use Drupal\Core\Block\Attribute\Block;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Session\AccountProxyInterface;
@@ -28,7 +29,7 @@ class WeatherApiBlock extends BlockBase implements ContainerFactoryPluginInterfa
   /**
    * Constructs a WeatherBlock objects.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, protected WeatherApiConnectionInterface $weatherApi, protected EntityTypeManagerInterface $entityTypeManager, protected WeatherDatabaseConnectionInterface $weatherDatabase, protected AccountProxyInterface $currentUser) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, protected WeatherApiConnectionInterface $weatherApi, protected EntityTypeManagerInterface $entityTypeManager, protected WeatherDatabaseConnectionInterface $weatherDatabase, protected AccountProxyInterface $currentUser, protected CacheBackendInterface $cacheBackend) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
   }
 
@@ -43,7 +44,8 @@ class WeatherApiBlock extends BlockBase implements ContainerFactoryPluginInterfa
       $container->get('weather_api.weather_connection'),
       $container->get('entity_type.manager'),
       $container->get('weather_api.weather_database'),
-      $container->get('current_user')
+      $container->get('current_user'),
+      $container->get('cache.default')
     );
   }
 
@@ -51,6 +53,7 @@ class WeatherApiBlock extends BlockBase implements ContainerFactoryPluginInterfa
    * {@inheritDoc}
    */
   public function build():array {
+
     $uid = $this->currentUser->id();
     $is_empty = $this->weatherDatabase->isEmptyRow($uid);
     if (!$is_empty) {
@@ -65,14 +68,25 @@ class WeatherApiBlock extends BlockBase implements ContainerFactoryPluginInterfa
       $city = 'Lutsk';
       $units = UnitsEnum::DegreesCelsius->value;
     }
-    $weatherData = $this->weatherApi->getWeatherApi($city, $units);
 
-    if (empty($weatherData)) {
-      return [];
+    $cache_id = 'weather_api_' . $city . '_' . $units;
+
+    if (!$cache = $this->cacheBackend->get($cache_id)) {
+      $api_data = $this->weatherApi->getWeatherApi($city, $units);
+
+      if (empty($api_data)) {
+        return [];
+      }
+
+      $this->cacheBackend
+        ->set($cache_id, $api_data, time() + 1800);
+    }
+    else {
+      $api_data = $cache->data;
     }
 
-    $temp = round($weatherData['main']['temp']);
-    $weather_text = $weatherData['weather'][0]['main'];
+    $temp = round($api_data['main']['temp']);
+    $weather_text = $api_data['weather'][0]['main'];
 
     return [
       '#theme' => 'weather_api_block',
@@ -81,13 +95,6 @@ class WeatherApiBlock extends BlockBase implements ContainerFactoryPluginInterfa
       '#weather_text' => $weather_text,
       '#units' => $units,
     ];
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function getCacheMaxAge():int {
-    return 1800;
   }
 
 }
